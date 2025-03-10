56b0cb975a19100660c57f5efc3838bc42ff9f2b
Rahil-B <rahilsrb@gmail.com>
2025-03-11 02:31:06 +0530

    reranking context documents

--- utils/embedding_utils.py
+++ utils/embedding_utils.py
@@ -2,13 +2,16 @@
 import streamlit as st
 import chromadb
 from langchain_huggingface import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer, CrossEncoder
 from config import CHROMA_DB_PATH, CHROMA_COLLECTION_NAME
 
 # Cache expensive resources using streamlit's caching
 @st.cache_resource
 def load_embedding_model():
     return HuggingFaceEmbeddings(model_name="sentence-transformers/multi-qa-MiniLM-L6-cos-v1")#(model_name="sentence-transformers/all-mpnet-base-v2")
-
+@st.cache_resource
+def load_rerank_model():
+    return CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
 @st.cache_resource
 def get_chroma_collection():
     client = chromadb.PersistentClient(path=CHROMA_DB_PATH)
@@ -18,22 +21,28 @@ def get_chroma_collection():
     )
     return collection
 embedding_model = load_embedding_model()
+reranker = load_rerank_model()
 def add_documents_to_collection(text_chunks):
     collection = get_chroma_collection()
     embeddings = embedding_model.embed_documents(text_chunks)
     ids = [str(idx) for idx in range(len(text_chunks))]
     collection.add(ids=ids, documents=text_chunks, embeddings=embeddings)
 
-def query_chroma(query, threshold=0.9):
+def query_chroma(query, threshold=0.7):
     collection = get_chroma_collection()
     query_embedding = embedding_model.embed_query(query)
     results = collection.query(query_embeddings=[query_embedding], n_results=5)
+
     
     if not results['documents'][0]:
         return None
-
-    scores = results['distances'][0]  # Lower values mean more similarity.
-    valid_contexts = [
-        doc for doc, score in zip(results['documents'][0], scores) if score < threshold
-    ]
+    cross_inp = [[query, hit] for hit in results['documents'][0]]
+    cross_scores = reranker.predict(cross_inp)
+    indexes = [idx for idx, score in enumerate(cross_scores) if score > 0]
+    valid_contexts = [results['documents'][0][idx] for idx in indexes]
+    
+    # scores = results['distances'][0]  # Lower values mean more similarity.
+    # valid_contexts = [
+    #     doc for doc, score in zip(results['documents'][0], scores) if score < threshold
+    # ]
     return valid_contexts if valid_contexts else None
